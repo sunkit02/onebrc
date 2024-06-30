9f9f72a68a6692a6dc67cb05c768629125c027aa
Sun Kit Tsui <sunkittsui@gmail.com>
2024-06-30 00:37:25 -0700

    Add benchmark for `custom_parse_float`

---
+++ benches/custom_parse_float.rs
@@ -0,0 +1,17 @@
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use onebrc::custom_parse_float;
+
+fn criterion_benchmark(c: &mut Criterion) {
+    let file_content = std::fs::read_to_string("./data/head_one_mil_temps.txt").unwrap();
+    let float_bytes = file_content
+        .split('\n')
+        .map(|s| s.as_bytes())
+        .collect::<Vec<&[u8]>>();
+
+    c.bench_function("custom_parse_float head_one_mil", |b| {
+        black_box(b.iter(|| float_bytes.iter().map(|&bytes| custom_parse_float(bytes))))
+    });
+}
+
+criterion_group!(benches, criterion_benchmark);
+criterion_main!(benches);
