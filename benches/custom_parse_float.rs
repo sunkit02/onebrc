8f3320267b6aace36858789acaa60a56822e081a
Sun Kit Tsui <sunkittsui@gmail.com>
2024-07-04 17:49:46 -0700

    Add baseline benchmark for float parsing using `FromStr` of `f64`

--- benches/custom_parse_float.rs
+++ benches/custom_parse_float.rs
@@ -3,6 +3,16 @@ use onebrc::custom_parse_float;
 
 fn criterion_benchmark(c: &mut Criterion) {
     let file_content = std::fs::read_to_string("./data/head_one_mil_temps.txt").unwrap();
+    let float_bytes = file_content.split('\n').collect::<Vec<&str>>();
+
+    c.bench_function("std_parse_float head_one_mil", |b| {
+        black_box(b.iter(|| {
+            float_bytes
+                .iter()
+                .map(|&float_str| float_str.parse::<f64>().unwrap())
+        }))
+    });
+
     let float_bytes = file_content
         .split('\n')
         .map(|s| s.as_bytes())
